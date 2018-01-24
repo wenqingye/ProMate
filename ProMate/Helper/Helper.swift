352c5599b1f4673c90b0cc417e2dd963d0c11cbb
Xin Liu <xliu159@hawk.iit.edu>
2018-01-23 23:36:33 -0600

    update helper

--- ProMate/Helper/Helper.swift
+++ ProMate/Helper/Helper.swift
@@ -9,5 +9,15 @@ extension UIButton {
 	}
 }
 
-
+extension UIViewController{
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
 
