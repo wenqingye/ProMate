8f34c74c8dd07dc9e236fa1f3cb7167cb5b25ea0
Wenqing Ye <jessie.ywq7@gmail.com>
2018-01-23 23:18:30 -0600

    Initial Commit

---
+++ ProMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProMate
+//
+//  Created by Wenqing Ye on 1/23/18.
+//  Copyright © 2018 Wenqing Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
