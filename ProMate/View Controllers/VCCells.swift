ab200b5d09254fd6eea545dd71a974efbf299abb
Wenqing Ye <jessie.ywq7@gmail.com>
2018-01-24 23:00:04 -0600

    add project and task related view controllers

--- ProMate/View Controllers/VCCells.swift
+++ ProMate/View Controllers/VCCells.swift
@@ -7,3 +7,18 @@ class ProjectCell: UITableViewCell {
 	@IBOutlet weak var projectNameLabel: UILabel!
 	@IBOutlet weak var managerNameLabel: UILabel!
 }
+
+class TaskCell: UITableViewCell {
+	
+	@IBOutlet weak var taskNameLabel: UILabel!
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var markButton: UIButton!
+}
+
+class UserCell: UITableViewCell {
+	
+	@IBOutlet weak var profileImage: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var emailLabel: UILabel!
+	@IBOutlet weak var selectButton: UIButton!
+}
