08eeb3e6aebcf6c5228d8d0012f09ef4bf2fcb1f
Xin Liu <xliu159@hawk.iit.edu>
2018-01-26 14:58:24 -0600

    update developer page

--- ProMate/View Controllers/HomeVC.swift
+++ ProMate/View Controllers/HomeVC.swift
@@ -3,36 +3,36 @@ import UIKit
 import Firebase
 
 class HomeVC: UIViewController {
-	
-	// MARK: - Properties
-	var projects: [Project] = []
-	var databaseRef: DatabaseReference?
-	@IBOutlet weak var tblView: UITableView!
-	//for developer, user dict map project to task assigned to current developer
+    
+    // MARK: - Properties
+    var projects: [Project] = []
+    var databaseRef: DatabaseReference?
+    @IBOutlet weak var tblView: UITableView!
+    //for developer, user dict map project to task assigned to current developer
     var taskDict = [String : [String]]()
-	
-	// MARK: - ViewController LifeCycle
+    
+    // MARK: - ViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-		tblView.estimatedRowHeight = 65
-		tblView.rowHeight = UITableViewAutomaticDimension
-		databaseRef = Database.database().reference()
-		
+        tblView.estimatedRowHeight = 65
+        tblView.rowHeight = UITableViewAutomaticDimension
+        databaseRef = Database.database().reference()
+        
         setUpInfo()
-		
+        
     }
 
-	
-	// MARK: - Methods
-	func setupUI() {
-		
-		navigationItem.title = "Home"
-		// if is developer, don't show the add project button
-		if AccessFirebase.sharedAccess.curUserInfo?.role != "manager" {
-			navigationItem.rightBarButtonItem = nil
-		}
-	}
-		
+    
+    // MARK: - Methods
+    func setupUI() {
+        
+        navigationItem.title = "Home"
+        // if is developer, don't show the add project button
+        if AccessFirebase.sharedAccess.curUserInfo?.role != "manager" {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+        
     
     func setUpInfo(){
         if AccessFirebase.sharedAccess.curUserInfo == nil{
@@ -43,30 +43,30 @@ class HomeVC: UIViewController {
             self.getProjects()
         }
     }
-	
-	func getProjects() {
-		
-		projects = []
-		if AccessFirebase.sharedAccess.curUserInfo?.role == "manager" {
-			// is manager, get projects ids, get projects
-			if let projectsIds = AccessFirebase.sharedAccess.curUserProjects {
-				for projectId in projectsIds {
-					// for each project id, get project object
+    
+    func getProjects() {
+        
+        projects = []
+        if AccessFirebase.sharedAccess.curUserInfo?.role == "manager" {
+            // is manager, get projects ids, get projects
+            if let projectsIds = AccessFirebase.sharedAccess.curUserProjects {
+                for projectId in projectsIds {
+                    // for each project id, get project object
                       AccessFirebase.sharedAccess.getProject(id: projectId){ (project) in
-						self.projects.append(project)
-						self.tblView.reloadData()
-					}
-				}
-			}
-		} else {
-			// is developer, get tasks ids
-			if let tasksIds = AccessFirebase.sharedAccess.curUserTasks {
-				for taskId in tasksIds {
-					// for each task id, get project id and then get project object
-					AccessFirebase.sharedAccess.getTask(id: taskId, completion: { (task) in
-						let projectId = task.projectId
-						AccessFirebase.sharedAccess.getProject(id: projectId, completion: { (project) in
-							self.projects.append(project)
+                        self.projects.append(project)
+                        self.tblView.reloadData()
+                    }
+                }
+            }
+        } else {
+            // is developer, get tasks ids
+            if let tasksIds = AccessFirebase.sharedAccess.curUserTasks {
+                for taskId in tasksIds {
+                    // for each task id, get project id and then get project object
+                    AccessFirebase.sharedAccess.getTask(id: taskId, completion: { (task) in
+                        let projectId = task.projectId
+                        AccessFirebase.sharedAccess.getProject(id: projectId, completion: { (project) in
+                            self.projects.append(project)
                             if let taskArr = self.taskDict[projectId]{
                                 var newArr = taskArr
                                 newArr.append(taskId)
@@ -75,82 +75,85 @@ class HomeVC: UIViewController {
                                 self.taskDict[projectId] = [taskId]
                             }
                             
-							self.tblView.reloadData()
-						})
-					})
-				}
-			}
-		}
-	}
-	
-	
-	// MARK: - Button Actions
-	@IBAction func didClickAddProject(_ sender: UIBarButtonItem) {
-		
-		if let curUser = AccessFirebase.sharedAccess.curUserInfo {
-			// prompt an alert box to enter project name to create a new project
-			let alertController = UIAlertController(title: "New Project Name", message: "", preferredStyle: .alert)
-			alertController.addTextField { (textfield) in
-				textfield.placeholder = "Name"
-			}
-			let saveAction = UIAlertAction(title: "Save", style: .default) { (alert) in
-				let nameTextfield = alertController.textFields![0]
-				if let name = nameTextfield.text {
-					// add new project to firebase
-					guard let projectId = self.databaseRef?.child("projects").childByAutoId().key else {
-						return
-					}
-					let projectDict = ["name": name, "id": projectId, "managerId": curUser.id] as [String: Any]
-					self.databaseRef?.child("projects").child(projectId).updateChildValues(projectDict)
-					
-					// add project to the user
-					self.databaseRef?.child("users").child(curUser.id).child("projects").updateChildValues([projectId: "1"])
-					
-					// update UI
-					let project = Project(name: name, id: projectId, tasksIds: [], managerId: curUser.id)
-					self.projects.append(project)
+                            self.tblView.reloadData()
+                        })
+                    })
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - Button Actions
+    @IBAction func didClickAddProject(_ sender: UIBarButtonItem) {
+        
+        if let curUser = AccessFirebase.sharedAccess.curUserInfo {
+            // prompt an alert box to enter project name to create a new project
+            let alertController = UIAlertController(title: "New Project Name", message: "", preferredStyle: .alert)
+            alertController.addTextField { (textfield) in
+                textfield.placeholder = "Name"
+            }
+            let saveAction = UIAlertAction(title: "Save", style: .default) { (alert) in
+                let nameTextfield = alertController.textFields![0]
+                if let name = nameTextfield.text {
+                    // add new project to firebase
+                    guard let projectId = self.databaseRef?.child("projects").childByAutoId().key else {
+                        return
+                    }
+                    let projectDict = ["name": name, "id": projectId, "managerId": curUser.id] as [String: Any]
+                    self.databaseRef?.child("projects").child(projectId).updateChildValues(projectDict)
+                    
+                    // add project to the user
+                    self.databaseRef?.child("users").child(curUser.id).child("projects").updateChildValues([projectId: "1"])
+                    
+                    // update UI
+                    let project = Project(name: name, id: projectId, tasksIds: [], managerId: curUser.id)
+                    self.projects.append(project)
                     DispatchQueue.main.async {
                         self.tblView.reloadData()
                     }
-				}
-			}
-			let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-			alertController.addAction(saveAction)
-			alertController.addAction(cancelAction)
-			present(alertController, animated: true, completion: nil)
-		}
-	}
+                }
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertController.addAction(saveAction)
+            alertController.addAction(cancelAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 }
 
 
 // MARK: - UITableViewDelegate & UITableViewDataSource
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		
-		return projects.count
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		
-		let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell") as! ProjectCell
-		let project = projects[indexPath.row]
-		
-		cell.projectNameLabel.text = project.name
-		
-		return cell
-	}
-	
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		
-		// pass the selected project to task vc
-		let project = projects[indexPath.row]
-		if let vc = storyboard?.instantiateViewController(withIdentifier: "TaskVC") as? TaskVC {
-			vc.project = project
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return projects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell") as! ProjectCell
+        let project = projects[indexPath.row]
+        
+        cell.projectNameLabel.text = project.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // pass the selected project to task vc
+        let project = projects[indexPath.row]
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "TaskVC") as? TaskVC {
+            vc.project = project
             vc.developerTasks = taskDict[project.id]
-			navigationController?.pushViewController(vc, animated: true)
-		}
-	}
-	
-	
+            let backBarButton = UIBarButtonItem(image: UIImage(named: "backWhiteButton"), style: .plain, target: nil, action: nil)
+            backBarButton.tintColor = .white
+            navigationItem.backBarButtonItem = backBarButton
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
 }
--- ProMate/View Controllers/TaskVC.swift
+++ ProMate/View Controllers/TaskVC.swift
@@ -4,70 +4,70 @@ import Firebase
 import SDWebImage
 
 class TaskVC: UIViewController {
-	
-	// MARK: - Properties
-	var project: Project?
-	var tasks: [Task] = []
+    
+    // MARK: - Properties
+    var project: Project?
+    var tasks: [Task] = []
     var developerTasks : [String]?
-	var databaseRef: DatabaseReference?
-	@IBOutlet weak var projectNameLabel: UILabel!
-	@IBOutlet weak var managerProfileImage: UIImageView!
-	@IBOutlet weak var managerNameLabel: UILabel!
-	@IBOutlet weak var tblView: UITableView!
-	
-	
-	// MARK: - ViewController LifeCycle
+    var databaseRef: DatabaseReference?
+    @IBOutlet weak var projectNameLabel: UILabel!
+    @IBOutlet weak var managerProfileImage: UIImageView!
+    @IBOutlet weak var managerNameLabel: UILabel!
+    @IBOutlet weak var tblView: UITableView!
+    
+    
+    // MARK: - ViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		databaseRef = Database.database().reference()
+        databaseRef = Database.database().reference()
         tblView.delegate = self
         tblView.dataSource = self
         setupUI()
-		fillInfo()
-		setupTasks()
+        fillInfo()
+        setupTasks()
     }
-	
-	
-	// MARK: - Methods
-	func setupUI() {
-		
-		navigationItem.title = "Task"
-		projectNameLabel.sizeToFit()
-		managerProfileImage.asCircle()
-		managerNameLabel.sizeToFit()
-		
-		let rightBarButton = UIBarButtonItem(image: UIImage(named: "addWhiteButton"), style: .plain, target: self, action: #selector (didClickAddTask))
-		rightBarButton.tintColor = .white
-		if AccessFirebase.sharedAccess.curUserInfo?.role == "manager" {
-			navigationItem.rightBarButtonItem = rightBarButton
-		} else {
-			navigationItem.rightBarButtonItem = nil
-		}
-	}
-	
-	func fillInfo() {
-		
-		// get manager info by the project manager id
-		if let project = project {
-			projectNameLabel.text = project.name
-			if let curUser = AccessFirebase.sharedAccess.curUserInfo {
-				if curUser.role == "manager" {
-					managerNameLabel.text = curUser.name
-					let url = URL(string: curUser.profilePic)
-					managerProfileImage.sd_setImage(with: url, completed: nil)
-				} else {
-					let managerId = project.managerId
-					AccessFirebase.sharedAccess.getUser(id: managerId, completion: { (user) in
-						self.managerNameLabel.text = user.name
-						let url = URL(string: user.profilePic)
-						self.managerProfileImage.sd_setImage(with: url, completed: nil)
-					})
-				}
-			}
-		}
-	}
-	
+    
+    
+    // MARK: - Methods
+    func setupUI() {
+        
+        navigationItem.title = "Task"
+        projectNameLabel.sizeToFit()
+        managerProfileImage.asCircle()
+        managerNameLabel.sizeToFit()
+        
+        let rightBarButton = UIBarButtonItem(image: UIImage(named: "addWhiteButton"), style: .plain, target: self, action: #selector (didClickAddTask))
+        rightBarButton.tintColor = .white
+        if AccessFirebase.sharedAccess.curUserInfo?.role == "manager" {
+            navigationItem.rightBarButtonItem = rightBarButton
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    func fillInfo() {
+        
+        // get manager info by the project manager id
+        if let project = project {
+            projectNameLabel.text = project.name
+            if let curUser = AccessFirebase.sharedAccess.curUserInfo {
+                if curUser.role == "manager" {
+                    managerNameLabel.text = curUser.name
+                    let url = URL(string: curUser.profilePic)
+                    managerProfileImage.sd_setImage(with: url, completed: nil)
+                } else {
+                    let managerId = project.managerId
+                    AccessFirebase.sharedAccess.getUser(id: managerId, completion: { (user) in
+                        self.managerNameLabel.text = user.name
+                        let url = URL(string: user.profilePic)
+                        self.managerProfileImage.sd_setImage(with: url, completed: nil)
+                    })
+                }
+            }
+        }
+    }
+    
     func getTasks(tasksIds : [String]) {
 
         // get tasks object by project tasks ids
@@ -96,82 +96,82 @@ class TaskVC: UIViewController {
             }
         }
     }
-	
-	
-	// MARK: - Button Actions
-	@objc func didClickAddTask() {
-		
-		if let vc = storyboard?.instantiateViewController(withIdentifier: "AddTaskVC") as? AddTaskVC {
-			vc.delegate = self
+    
+    
+    // MARK: - Button Actions
+    @objc func didClickAddTask() {
+        
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "AddTaskVC") as? AddTaskVC {
+            vc.delegate = self
             vc.curProject = self.project
             navigationController?.pushViewController(vc, animated: true)
             //navigationController?.present(vc, animated: true, completion: nil)
-		}
-	}
-	
-	@IBAction func didClickFinishTask(_ sender: UIButton) {
-		
-		sender.isSelected = !sender.isSelected
-		let index = sender.tag
-		var task = tasks[index]
-		if sender.isSelected == true {
-			task.isFinished = true
-			databaseRef?.child("tasks").child(task.id).updateChildValues(["isFinished": true])
-		} else {
-			task.isFinished = false
-			databaseRef?.child("tasks").child(task.id).updateChildValues(["isFinished": false])
-		}
-	}
+        }
+    }
+    
+    @IBAction func didClickFinishTask(_ sender: UIButton) {
+        
+        sender.isSelected = !sender.isSelected
+        let index = sender.tag
+        var task = tasks[index]
+        if sender.isSelected == true {
+            task.isFinished = true
+            databaseRef?.child("tasks").child(task.id).updateChildValues(["isFinished": true])
+        } else {
+            task.isFinished = false
+            databaseRef?.child("tasks").child(task.id).updateChildValues(["isFinished": false])
+        }
+    }
 }
 
 
 // MARK: - UITableViewDelegate & UITableViewDataSource
 extension TaskVC: UITableViewDelegate, UITableViewDataSource {
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		
-		return tasks.count
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		
-		let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") as! TaskCell
-		let task = tasks[indexPath.row]
-		
-		cell.taskNameLabel.text = task.name
-		cell.dateLabel.text = "\(task.startDate) - \(task.endData)"
-		if task.isFinished == true {
-			cell.markButton.isSelected = true
-		} else {
-			cell.markButton.isSelected = false
-		}
-		cell.markButton.tag = indexPath.row
-		
-		return cell
-	}
-	
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		
-		let task = tasks[indexPath.row]
-		if let vc = storyboard?.instantiateViewController(withIdentifier: "TaskDetailsVC") as? TaskDetailsVC {
-			vc.task = task
-			navigationController?.pushViewController(vc, animated: true)
-		}
-	}
-	
-	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		
-		tblView.estimatedRowHeight = 80
-		return UITableViewAutomaticDimension
-	}
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") as! TaskCell
+        let task = tasks[indexPath.row]
+        
+        cell.taskNameLabel.text = task.name
+        cell.dateLabel.text = "\(task.startDate) - \(task.endData)"
+        if task.isFinished == true {
+            cell.markButton.isSelected = true
+        } else {
+            cell.markButton.isSelected = false
+        }
+        cell.markButton.tag = indexPath.row
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let task = tasks[indexPath.row]
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "TaskDetailsVC") as? TaskDetailsVC {
+            vc.task = task
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        tblView.estimatedRowHeight = 80
+        return UITableViewAutomaticDimension
+    }
 }
 
 
 // MARK: - AddNewTask
 extension TaskVC: AddNewTask {
-	
-	func didAddNewTask(newTask: Task) {
-		tasks.append(newTask)
-		tblView.reloadData()
-	}	
+    
+    func didAddNewTask(newTask: Task) {
+        tasks.append(newTask)
+        tblView.reloadData()
+    }    
 }
